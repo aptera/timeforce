bcba43a59dd80c27911af6b1b63db80f7f7ac2cf
Nathan Buuck <nbuuck@gmail.com>
2018-07-14 19:40:05 -0400

    maint: minimize catchall handler.

--- index.js
+++ index.js
@@ -102,29 +102,7 @@ controller.hears('log', 'direct_message', function(bot, message){
     });
 });
 
+// LAST handler. Catchall to display supported commands.
 controller.on('direct_message', function(bot, message){
-    if(!message.text.match('^log .+')){
-        writeHelpText(bot, message);
-    }
-});
-
-function writeHelpText(bot, message){
     bot.reply(message, "Sorry, I didn't understand that. Here are the commands I support:\n`log [text to log]`");
-}
-
-/**
- * AN example of what could be:
- * Any un-handled direct mention gets a reaction and a pat response!
- */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
+});
